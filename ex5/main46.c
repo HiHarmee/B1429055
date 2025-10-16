1c21ce9a725b47628a4fe254db0d0b85da2bf44f
HiHarmee <61413014+HiHarmee@users.noreply.github.com>
2025-10-16 15:53:24 +0800

    Create main46.c

---
+++ ex5/main46.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void towerofHanoi(int n,char from_rod,char to_rod,char aux rod)
+if (n==1){
+print("\n move disk 1 from rod %c to rod %c, from_rod,to_rod);
+return;}
+towerofHanooi(n-1,from_rod,aux_rod,to_rod);
+print("\n move disk 1 from rod %c to rod %c, from_rod,to_rod);
+toerofHanoi(n-1,aux_rod,to_rod,from_rod);
+}
+int main(){
+int n= 3;
+towerHanoi(n,"a","b","c")
+return 0;
+)
